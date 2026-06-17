65f083e63e22ae167acc9db9055898fe3dab8934
LeonWu <leon@lokinfo.com>
2026-06-17 10:50:12 +0800

    chore(release): prepare v1.6.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "top.yogiczy.mytv"
         minSdk = 21
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.6.3"
+        versionCode = 6
+        versionName = "1.6.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
